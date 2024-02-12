9cd380f775bdf57de704dd33dcfd7ae92d17bfa3
Robert Moses <rmoses@titaniumbytes.com>
2024-02-12 12:08:55 -0600

    added config repo

---
+++ scripts/custom_dot_files.sh
@@ -0,0 +1,8 @@
+git clone git@github.com:robbymoses/linux_config_files.git ~/
+
+# ZSH
+ln -s ~/custom_dot_files/zsh/.zshrc ~/.zshrc
+# NVIM
+
+# TMUX
+
--- scripts/zap_zsh.sh
+++ scripts/zap_zsh.sh
@@ -3,9 +3,3 @@ echo "Installing Zap ZSH..."
 chsh -s /bin/zsh ${USER}
 # Install Zap
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1 -keep
-# Pull ZSH Config Repo
-git clone git@github.com:robbymoses/config_zsh.git ~/.config/zsh
-# Remove .zshrc file from home
-rm ~/.zshrc
-# Create Symlink from config repo to home
-ln -s ~/.config/zsh/.zshrc ~/.zshrc 
