8c2b50541541563f665a862c2986e274e3ad884b
Robert Moses <rmoses@titaniumbytes.com>
2024-02-12 20:05:54 -0600

    Added DbVisualzier

--- scripts/dbvisualizer.sh
+++ scripts/dbvisualizer.sh
@@ -1,5 +1,6 @@
-CURRENT_DBVIS_VERSION_URL=https://www.dbvis.com/product_download/dbvis-24.1.1/media/dbvis_linux_24_1_1.tar.gz
+CURRENT_DBVIS_VERSION_URL=https://www.dbvis.com/product_download/dbvis-24.1.2/media/dbvis_linux_24_1_2.rpm
+DBVIS_RPM_PATH=~/rpm/dbvis_24_1_12.rpm
 
-curl -o ~/Downloads/dbvis.tar.gz $CURRENT_DBVIS_VERSION_URL
+wget -O $DBVIS_RPM_PATH $CURRENT_DBVIS_VERSION_URL
 
-
+sudo rpm -i $DBVIS_RPM_PATH
