6794c3defa286d428f1c10c91d49bbd22b2cf8bb
Robert Moses <rmoses@titaniumbytes.com>
2024-02-12 22:37:45 -0600

    added p flag for mkdir

--- scripts/dbvisualizer.sh
+++ scripts/dbvisualizer.sh
@@ -2,7 +2,7 @@ CURRENT_DBVIS_VERSION_URL=https://www.dbvis.com/product_download/dbvis-24.1.2/me
 DBVIS_RPM_NAME=dbvis_24_1_2.rpm
 DBVIS_RPM_PATH=~/rpm/dbvis
 
-mkdir $DBVIS_RPM_PATH
+mkdir -p $DBVIS_RPM_PATH
 wget -O $DBVIS_RPM_PATH/$DBVIS_RPM_NAME $CURRENT_DBVIS_VERSION_URL
 
 sudo rpm -i $DBVIS_RPM_PATH/$DBVIS_RPM_NAME
