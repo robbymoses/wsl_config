9076a3a60010eff1e5160d2ce9f1cf2d84a8011f
Robert Moses <rmoses@titaniumbytes.com>
2024-02-12 20:41:09 -0600

    Fixed DBVis Script

--- scripts/dbvisualizer.sh
+++ scripts/dbvisualizer.sh
@@ -1,6 +1,8 @@
 CURRENT_DBVIS_VERSION_URL=https://www.dbvis.com/product_download/dbvis-24.1.2/media/dbvis_linux_24_1_2.rpm
-DBVIS_RPM_PATH=~/rpm/dbvis_24_1_12.rpm
+DBVIS_RPM_NAME=dbvis_24_1_2.rpm
+DBVIS_RPM_PATH=~/rpm/dbvis
 
-wget -O $DBVIS_RPM_PATH $CURRENT_DBVIS_VERSION_URL
+mkdir $DBVIS_RPM_PATH
+wget -O $DBVIS_RPM_PATH/$DBVIS_RPM_NAME $CURRENT_DBVIS_VERSION_URL
 
-sudo rpm -i $DBVIS_RPM_PATH
+sudo rpm -i $DBVIS_RPM_PATH/$DBVIS_RPM_NAME
